10e631782cb74c0d0f4f56869a0ce088c243380e
sowmya <macharlasowmya8@gmail.com>
2026-01-31 20:56:32 +0530

    Project

--- routes/trip.routes.js
+++ routes/trip.routes.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const router=express.Router();
+const{
+    createTrip,
+    endTrip}
+    =require("../controllers/trip.controller");
+    router.post("/create",createTrip);
+    router.post("/end/:id",endTrip);
+
+
+module.exports=router;
