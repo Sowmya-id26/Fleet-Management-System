4685eb6c61af954ff011ef33f9502d3a810c3e98
sowmya <macharlasowmya8@gmail.com>
2026-01-31 21:03:20 +0530

    Project

--- config/supabase.js
+++ config/supabase.js
@@ -2,7 +2,7 @@ const { createClient }=
 require("@supabase/supabase.js");
 require("dotenv").config();
 const supabase=createClient(
-    process.env.url,
-    process.env.key
+    process.env.https://zdcgpbllrahdzreciykc.supabase.co,
+    process.env.sb_publishable_JJCx-rrhFnZcc78MtJzhDw_jMCVneqF
 );
 module.exports=supabase;
